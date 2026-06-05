5dcaa04d7e5592143727806dcbbf2e1abeca42b1
!!Dean <dwhall256@gmail.com>
2026-06-04 21:13:28 -0500

    Update scheduler
    Most importantly, update the register access
    to match minisvd2nim

--- src/krnlpkg/scheduler.nim
+++ src/krnlpkg/scheduler.nim
@@ -11,31 +11,24 @@
 ## when only one task is scheduled or by writing to multiple NVIC.NVIC_ISPR[]
 ## registers when multiple tasks are scheduled.
 ## The NVIC runs the highest priority pending interrupt and will tail-chain
-## to the next highest priority pending interrupt when the first one finishes.
+## to the next highest priority pending interrupt when the current one finishes.
 ##
 
 import std/math
-# TODO: this should be a common import for all ArmV7M and ArmV8M cores:
-import cm4f/[nvic, sig]
+import armv7m/[nvic, sig]
+import nrf52840/device
+import bitflags, types
 
 const PLAT_IRQ_CNT = 256 # placeholder; shold come from device import
 
-type # TODO: this should come from the ARM core import
+type
+  # TODO: this should come from the ARM core import
   InterruptNmbr = uint16
-
-type # TODO: these should come from signal registry
-  Bitflags[N: static uint16] = object
-    bits: array[N.ceilDiv (8 * sizeof uint32), uint32]
-
-  TaskSet = Bitflags[256]
-
-type # TODO: this should come from the task (common?) import
-  Task = object
-    irqNmbr: int
-
-type Scheduler = object
-  taskRegistry: array[PLAT_IRQ_CNT, ref Task]
-  enabled: array[PLAT_IRQ_CNT, bool]
+  # TODO: these should come from signal registry
+  TaskSet = Bitflags[PLAT_IRQ_CNT]
+  Scheduler = object
+    taskRegistry: array[PLAT_IRQ_CNT, ref Task]
+    enabled: array[PLAT_IRQ_CNT, bool]
 
 proc registerTask(schd: var Scheduler, task: ref Task) =
   ## Registers a task with this kernel
@@ -50,18 +43,17 @@ proc enable(schd: var Scheduler, task: ref Task) =
   ## Enable a task's interrupt so it can be scheduled
   let (regIdx, bitIdx) = task.irqNmbr.divmod 32
   schd.enabled[task.irqNmbr] = true
-  NVIC.NVIC_ISER[regIdx].SETENA[bitIdx](1).write()
+  NVIC.NVIC_ISER(regIdx).SETENA(bitIdx, 1)
 
 proc disable(schd: var Scheduler, task: ref Task) =
   ## Disable the task's interrupt so it won't be scheduled
   let (regIdx, bitIdx) = task.irqNmbr.divmod 32
   schd.enabled[task.irqNmbr] = false
-  NVIC.NVIC_ICER[regIdx].SETENA[bitIdx](1).write()
+  NVIC.NVIC_ICER(regIdx).SETENA(bitIdx, 1)
 
 proc schedule(schd: Scheduler, task: ref Task) =
   ## Schedule a single task to run by pending its interrupt
-  # FIXME: INTID field has no read access, so rmw won't work
-  SIG.STIR.INTID(task.irqNmbr).write()
+  SIG.STIR.INTID(task.irqNmbr)
 
 proc schedule(schd: Scheduler, taskset: TaskSet) =
   ## Schedule multiple tasks to run by pending their interrupts.
@@ -85,9 +77,10 @@ proc schedule(schd: Scheduler, taskset: TaskSet) =
     for idx, bundle in taskset.pairs:
       NVIC.NVIC_ISPR[idx] = bundle
 
+# TODO: add the naked pragma
 proc dispatch(schd: Scheduler, irqNmbr: static InterruptNmbr) =
   ## Dispatches the next event to the task with the given irqNmbr.
-  ## WARNING: This procedure is called in the handler context
+  ## ATTENTION: This procedure is called in the handler context
   const task = schd.getTask(irqNmbr)
   let
     # FIXME: put these in the named registers
@@ -99,4 +92,4 @@ proc dispatch(schd: Scheduler, irqNmbr: static InterruptNmbr) =
 proc dispatchIrq17(schd: Scheduler) =
   ## Declares a dispatch procedure for the task with irqNmbr 17.
   ## This procedure's only use is to be placed in the vector table.
-  dispatch(schd, 17)
+  schd.dispatch(17)
