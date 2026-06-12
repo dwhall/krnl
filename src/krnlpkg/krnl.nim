0770a74514337c8b220c0ea9eb480d548da9db0f
!!Dean <dwhall256@gmail.com>
2026-06-11 19:14:15 -0500

    Remove junk

--- src/krnlpkg/krnl.nim
+++ src/krnlpkg/krnl.nim
@@ -1,56 +1,13 @@
 ## Copyright 2024 Dean Hall See LICENSE for details
 ##
-## KRNL: an event-based microkernel with memory protection for ARM Cortex-M devices
-## This takes ideas from Miro Samek's Super-Simple-Tasker and QPc
-##   https://github.com/QuantumLeaps/
+## KRNL
 ##
-#!fmt: off
 
-import armv7m/[core, fp, nvic, scb]
-import debug_rtt, types, task
-
-type
-  Handler = proc(self: var Task, e: Evnt)
-  LockKey = uint32
-
-template CRIT_ENTER() =
-  disableIrq()
-
-template CRIT_EXIT() =
-  enableIrq()
+import armv7m/core
 
 proc init*() =
   discard
 
-func startTask*[N](task: var Task[N], prio: TaskPriority, initEvnt: Evnt) =
-  task.setPriority(prio)
-  task.init(initEvnt)
-
-func runForever*(appOnStart: proc) {.noreturn.} =
-  if appOnStart != nil:
-    appOnStart()
+func runForever*() {.noreturn.} =
   while true:
     WFI()
-
-####
-
-# I don't have a plan for these two procs yet
-#
-# proc lock*(ceiling: TaskPriority): LockKey =
-#   let nvicPrio: NvicPriority = ceiling
-#   result = BASEPRI.read()
-#   if result > nvicPrio:
-#     CRIT_ENTER()
-#     BASEPRI.write(nvicPrio)
-#     CRIT_EXIT()
-#
-# proc unlock*(lockKey: LockKey) =
-#   # NOTE: ARMv7-M+ support the BASEPRI register and the selective SST scheduler
-#   # unlocking is implemented by restoring BASEPRI to the lockKey level.
-#   BASEPRI.write(lockKey)
-#
-# func newTask*[T](eventQue: ptr RingQue, init: Handler, dispatch: Handler): Task[N] =
-#   result = Task[N]()
-#   result.eventQue = eventQue
-#   result.init = init
-#  result.dispatch = dispatch
