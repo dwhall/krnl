3157208d21dfd33fb1930969949719a58a974080
!!Dean <dwhall256@gmail.com>
2026-06-20 12:13:06 -0500

    Refactor for easier reading
    Also removed a redundant assert

--- src/krnlpkg/signal.nim
+++ src/krnlpkg/signal.nim
@@ -15,18 +15,16 @@ func Sig*(
 ): Signal {.compileTime.} =
   ## Composes a signal from the source locator (case insensitive) and signal enum
   when kind == SigShort:
-    assert sigEnum < 256
+    const
+      hashMask = 0x7FFF_FF00'u32
+      sigIdxMask = 0xFF'u32
+      sigKindBit = 0'u32
   else:
-    assert sigEnum < 1024
-  const
-    hashMask = when kind == SigShort: 0x7FFFFF00'u32 else: 0x7FFFFC00'u32
-    truncatedHash = locator.hash.uint32 and hashMask
-    sigIdxMask = when kind == SigShort: 0xFF'u32 else: 0x3FF'u32
-    sigKindBit =
-      when kind == SigShort:
-        0'u32
-      else:
-        1'u32 shl 31
+    const
+      hashMask = 0x7FFF_FC00'u32
+      sigIdxMask = 0x3FF'u32
+      sigKindBit = 0x8000_0000'u32
+  const truncatedHash = locator.hash.uint32 and hashMask
   assert sigEnum <= sigIdxMask
   Signal(sigKindBit or truncatedHash or sigEnum)
 
