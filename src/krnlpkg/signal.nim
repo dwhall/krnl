2e2409b8185d811d5d61fe5673ccb6277fd4f7c6
!!Dean <dwhall256@gmail.com>
2026-06-20 12:50:42 -0500

    make SourceLocator opaque
    This gives us the freedom to use various manners to construct
    a SourceLocator, as long as we provide a public converter.
    
    Eliminate Sig() that uses SourceLocator.

--- src/krnlpkg/signal.nim
+++ src/krnlpkg/signal.nim
@@ -5,33 +5,26 @@
 import source_locator, types
 
 type SignalKind* = enum
-  SigShort = 0 # 23-bit source, 8-bit index
-  SigLong = 1 # 21-bit source, 10-bit index
-
-func Sig*(
-    locator: static SourceLocator,
-    sigEnum: static uint32,
-    kind: static SignalKind = SigShort,
-): Signal {.compileTime.} =
-  ## Composes a signal from the source locator (case insensitive) and signal enum
-  when kind == SigShort:
-    const
-      hashMask = 0x7FFF_FF00'u32
-      sigIdxMask = 0xFF'u32
-      sigKindBit = 0'u32
-  else:
-    const
-      hashMask = 0x7FFF_FC00'u32
-      sigIdxMask = 0x3FF'u32
-      sigKindBit = 0x8000_0000'u32
-  const truncatedHash = locator.hash.uint32 and hashMask
-  assert sigEnum <= sigIdxMask
-  Signal(sigKindBit or truncatedHash or sigEnum)
+  SigShort # 23-bit source locator, 8-bit index
+  SigLong # 21-bit source locator, 10-bit index
 
 func Sig*(
     dottedLocator: static string,
     sigEnum: static uint32,
     kind: static SignalKind = SigShort,
 ): Signal {.compileTime.} =
-  const locator = toSourceLocator(dottedLocator)
-  Sig(locator, sigEnum, kind)
+  ## Composes a signal from the dotted source locator (case insensitive) and signal enum,
+  ## where the dotted source locator is of the form: `"<domain>.<package>.<module>"`
+  when kind == SigShort:
+    const
+      hashMask = 0x7FFF_FF00'u32
+      sigEnumMask = 0xFF'u32
+  else:
+    const
+      hashMask = 0x7FFF_FC00'u32
+      sigEnumMask = 0x3FF'u32
+  assert sigEnum <= sigEnumMask, "sigEnum exceeds bitfield limit"
+  const
+    locator = toSourceLocator(dottedLocator)
+    truncatedHash = locator.hash.uint32 and hashMask
+  Signal((kind.uint32 shl 31) or truncatedHash or (sigEnum and sigEnumMask))
--- src/krnlpkg/source_locator.nim
+++ src/krnlpkg/source_locator.nim
@@ -3,11 +3,11 @@
 
 import std/[hashes, strutils]
 
-type SourceLocator* = object
-  ## Pathing information to uniquely identify named source code items
+type SourceLocator = object
+  ## Locator information to uniquely identify named source code items
   ## in a large project.  `domain` is the broadest, `package` is the
   ## middle scope, and `module` is the narrowest scope.  SourceLocator
-  ## is intended for use with identifying such items as Signals and Data
+  ## is intended to assist in uniquely identifying, i.e. Signals and Data
   domain: string
   package: string
   module: string
@@ -15,9 +15,9 @@ type SourceLocator* = object
 converter toSourceLocator*(dottedLocator: static string): SourceLocator =
   assert dottedLocator.count('.') == 2, "Expecting exactly two `.` in dottedLocator"
   assert dottedLocator.count(' ') == 0, "Expecting no spaces ` ` in dottedLocator"
-  # TODO: constrain to legit identifier charset
-  const sigSource = dottedLocator.split('.')
-  SourceLocator(domain: sigSource[0], package: sigSource[1], module: sigSource[2])
+  # TODO: validate as a legit identifier
+  const dotLocField = dottedLocator.split('.')
+  SourceLocator(domain: dotLocField[0], package: dotLocField[1], module: dotLocField[2])
 
 func hash*(locator: SourceLocator): Hash {.compileTime.} =
   ## A case-insensitive hash of domain, then package, then module
